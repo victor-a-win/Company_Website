bfbd007cc23e3624894d97e017c93c824b1644db
victor-a-win <victoradiwinata@gmail.com>
2025-09-08 23:00:25 +0800

    fix(axios && next.config): axios api user team

--- company_profile/next.config.ts
+++ company_profile/next.config.ts
@@ -10,6 +10,11 @@ const nextConfig: NextConfig = {
         pathname:
           "/787A5544-6A95-43BD-BB70-C046FDD5F600/AA32665B-0F88-49C6-9985-75AB6071F234/files/posts/**",
       },
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+        pathname: "/**",
+      },
     ],
     domains: ["randomuser.me"],
   },
